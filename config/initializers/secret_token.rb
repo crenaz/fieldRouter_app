e762213a0d958b93633addda081b378e6f018ca5
crenaz <crenaz@gmail.com>
2014-11-16 16:08:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FieldRouter::Application.config.secret_key_base = 'af12978531658dce23839728f827e6dbb80349ebf5431561af6c3d4c9041ff16989b646a6d4c323a14d496b56a8e996450990c787fbc57a298c8d2c10ceb1017'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FieldRouter::Application.config.session_store :cookie_store, key: '_fieldRouter_session'
